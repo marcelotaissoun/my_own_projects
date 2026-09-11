47594714d547d6084e92c4c6d32bf606d5acb685
Marcelo Taissoun <marcelotaissoun@gmail.com>
2026-09-11 20:35:20 -0300

    Create class StudentService

---
+++ Basic Projects/Student Register/StudentService.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class StudentService {
+
+    private ArrayList<Student> students = new ArrayList<>();
+
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    public void listStudents() {
+        for(Student student : students) {
+            System.out.println(student);
+        }
+    }
+
+    public Aluno searchById(int studentId) {
+        for(Student student : students){
+            if(student.getStudentId() == studentId) {
+               return student;
+            }
+        }
+      
+        return null;
+    }
+
+    public void removeStudent(int studentId) {
+         for(Student student : students){
+            if(student.getStudentId() == studentId) {
+               remove(student);
+            }
+         }
+    }
+}
