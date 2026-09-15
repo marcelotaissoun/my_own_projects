3e1b4d0c720f9fcdc6d405c240e65585bfe6427d
Marcelo Taissoun <marcelotaissoun@gmail.com>
2026-09-15 17:36:38 -0300

    Extend SavingsAccount from Account

---
+++ Basic Projects/Banking System/Models/SavingsAccount.java
@@ -0,0 +1,17 @@
+public class SavingsAccount extends Account {
+    public SavingsAccount(int accId, Client client) {
+        super(accId, client);
+    }
+
+    @Override
+    public void withdrawal(double value) {
+        if(value <= 0) {
+            throw new InvalidOpException("Withdrawal value must be greater than zero.");
+        }
+      
+        if(value > getAccBalance()) {
+            throw new InsuffBalanceException("Insufficient balance.");
+        }
+        doWithdrawal(value);
+    }
+}
