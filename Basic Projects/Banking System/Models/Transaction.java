7beec818e0395026cb08fdd5365df365fdb4d1a5
Marcelo Taissoun <marcelotaissoun@gmail.com>
2026-09-15 17:45:33 -0300

    Create class Transaction

---
+++ Basic Projects/Banking System/Models/Transaction.java
@@ -0,0 +1,25 @@
+import java.time.LocalDateTime;
+
+public class Transaction {
+    private TransactionType transacType;
+    private double transacValue;
+    private LocalDateTime transacDate;
+
+    public Transaction(TransactionType transacType, double transacValue) {
+        this.transacType = transacType;
+        this.transacValue = transacValue;
+        this.transacDate = LocalDateTime.now();
+    }
+
+    public TransactionType getTransacType() {
+        return transacType;
+    }
+
+    public double getTransacValue() {
+        return transacType;
+    }
+
+    public LocalDateTime getTransacDate() {
+        return transacDate;
+    }
+}
